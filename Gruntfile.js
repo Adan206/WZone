77abceaef5ca8aea42294261b286b2d3bc7102de
Adan206 <adan@Adans-MacBook-Pro.local>
2017-05-13 18:45:45 -0700

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,21 @@ module.exports = function (grunt) {
     // Project settings
     yeoman: appConfig,
 
+    buildcontrol: {
+     options: {
+       dir: 'dist',
+       commit: true,
+       push: true,
+       message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+     },
+     pages: {
+       options: {
+         remote: 'git@github.com:Adan206/WeatherZone.git',
+         branch: 'gh-pages'
+       }
+     }
+   },
+
     // Watches files for changes and runs tasks based on the changed files
     watch: {
       bower: {
